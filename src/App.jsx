8de047177b013c062d3db8baef663240c3ed8019
GaveshikaPerera <pereragaveshika@gmail.com>
2026-06-30 20:21:21 +0530

    Add About Us page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
+import AboutUsPage from "./pages/AboutUsPage";
 import GuideDashboardPage from "./pages/GuideDashboardPage";
 
 import { Toaster } from "react-hot-toast";
@@ -24,6 +25,8 @@ function App() {
         <Route path="/signin" element={<LoginPage />} />
 
         <Route path="/admin/*" element={<AdminPage />} />
+
+         <Route path="/about-us" element={<AboutUsPage />} />
 
         <Route path="/guide-dashboard" element={<GuideDashboardPage />} />
       </Routes>
